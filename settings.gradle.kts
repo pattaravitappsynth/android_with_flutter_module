37c089c1614a77e7e825130baab492f7632d66ed
suraking <pattaravit.k@outlook.com>
2023-05-30 10:23:44 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+apply(from = "flutter.gradle")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+}
+rootProject.name = "My Application"
+include(":app")
